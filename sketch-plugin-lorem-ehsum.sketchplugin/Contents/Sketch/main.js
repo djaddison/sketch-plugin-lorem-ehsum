b126194a3f096458e3620eba9d7926f442d23c22
David Addison <djaddison.vcs@gmail.com>
2018-07-05 15:20:35 -0400

    Added license header to source files

--- sketch-plugin-lorem-ehsum.sketchplugin/Contents/Sketch/main.js
+++ sketch-plugin-lorem-ehsum.sketchplugin/Contents/Sketch/main.js
@@ -1327,6 +1327,21 @@ module.exports = g;
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var lodash_shuffle__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash.shuffle */ "./node_modules/lodash.shuffle/index.js");
 /* harmony import */ var lodash_shuffle__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash_shuffle__WEBPACK_IMPORTED_MODULE_0__);
+/*
+ *  Copyright 2018 David Addison
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 
 var wordList = ['July 1', 'Canada Day', 'keener', 'aboot', 'eh', 'chirping', 'beaking', 'mickey', 'texas mickey', 'stag', 'stagette', 'give\'r', 'gong show', 'gonger', 'hang a larry', 'hang a roger', 'homo milk', 'two-four', 'KD', 'toque', 'out for a rip', 'pre-drinking', 'deking', 'dart', 'double-double', 'The Hortons', 'timbit', 'decked out', 'Nanaimo bar', 'Rockets', 'Freezies', 'runners', 'holiday', 'chesterfield', 'garburator', 'washroom', 'housecoat', 'pencil crayons', 'college', 'haligonian', 'parkade', 'Canuck', 'ten clicks', 'five clicks away', 'ten clicks out', 'hoser', 'kerfuffle', 'Molson muscle', 'lineup', 'for sure', 'arse', 'backbacon', 'icing sugar', 'whitener', 'processed cheese', 'chocolate bar', 'brown bread', 'rye & ginger', 'tutorial', 'marking', 'public school', 'supply teacher', '(z)ed', 'the bill', 'eavestrough', 'elastic', 'girl guides', 'hydro', 'serviette', 'tap', 'track pants', 'muskoka chair', 'postal code', 'beavertail', 'poutine', 'ketchup chips', 'vinegar on fries', 'butter tart', 'tortiere', 'milk in a bag', 'Smarties', 'Crispy Crunch', 'Coffee Crisp', 'Caramilk', 'Shreddies', '5-pin bowling', 'mountie', 'toonie', 'twoonie', 'David Wilcox', 'The Tragically Hip', 'Bare Naked Ladies', 'Maclean and Maclean'];
 /* harmony default export */ __webpack_exports__["default"] = (function (context) {
